9703e689cab3f4f7b31bd1d81fae9b2ae522ceaa
felipevcc <felipevillamizarc@gmail.com>
2023-06-18 16:53:59 -0500

    [Feature]: Swagger setup for documentation

--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -24,28 +24,33 @@ app.use(helmet());
 // Handle JSON
 app.use(express.json());
 
-// import routes
-import ApplicationsRoutes from '../routes/Applications.routes';
-import Company_profilesRoutes from '../routes/Company_profiles.routes';
-import ContactsRoutes from '../routes/Contacts.routes';
-import MessagesRoutes from '../routes/Messages.routes';
-import MultimediaRoutes from '../routes/Multimedia.routes';
-import Professional_profilesRoutes from '../routes/Professional_profiles.routes';
-import ProjectsRoutes from '../routes/Projects.routes';
-import RatingsRoutes from '../routes/Ratings.routes';
-import SkillsRoutes from '../routes/Skills.routes';
-import UsersRoutes from '../routes/Users.routes';
+// Import routes
+import ApplicationsRoutes from "../routes/Applications.routes";
+import Company_profilesRoutes from "../routes/Company_profiles.routes";
+import ContactsRoutes from "../routes/Contacts.routes";
+import MessagesRoutes from "../routes/Messages.routes";
+import MultimediaRoutes from "../routes/Multimedia.routes";
+import Professional_profilesRoutes from "../routes/Professional_profiles.routes";
+import ProjectsRoutes from "../routes/Projects.routes";
+import RatingsRoutes from "../routes/Ratings.routes";
+import SkillsRoutes from "../routes/Skills.routes";
+import UsersRoutes from "../routes/Users.routes";
 
-// use routes
-app.use('/api', ApplicationsRoutes);
-app.use('/api', Company_profilesRoutes);
-app.use('/api', ContactsRoutes);
-app.use('/api', MessagesRoutes);
-app.use('/api', MultimediaRoutes);
-app.use('/api', Professional_profilesRoutes);
-app.use('/api', ProjectsRoutes);
-app.use('/api', RatingsRoutes);
-app.use('/api', SkillsRoutes);
-app.use('/api', UsersRoutes);
+// Use routes
+app.use("/api", ApplicationsRoutes);
+app.use("/api", Company_profilesRoutes);
+app.use("/api", ContactsRoutes);
+app.use("/api", MessagesRoutes);
+app.use("/api", MultimediaRoutes);
+app.use("/api", Professional_profilesRoutes);
+app.use("/api", ProjectsRoutes);
+app.use("/api", RatingsRoutes);
+app.use("/api", SkillsRoutes);
+app.use("/api", UsersRoutes);
+
+// Documentation (Swagger)
+import swaggerUi from "swagger-ui-express";
+import swaggerSpecs from "../docs/swagger";
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
 export default app;
---
+++ src/docs/swagger.ts
@@ -0,0 +1,25 @@
+import swaggerJSDoc, { OAS3Definition, OAS3Options } from "swagger-jsdoc";
+
+const swaggerDefinition: OAS3Definition = {
+  openapi: "3.0.0",
+  info: {
+    title: "Coderise API",
+    version: "1.0.0",
+    description: "API for the Backend project at Coderise for a recruitment system between companies and students",
+  },
+  servers: [
+    {
+      url: "http://localhost:3000",
+      description: "Development server"
+    },
+  ]
+};
+
+const options: OAS3Options = {
+  swaggerDefinition,
+  apis: ["../routes/*.ts"],
+};
+
+const swaggerSpecs = swaggerJSDoc(options);
+
+export default swaggerSpecs;
